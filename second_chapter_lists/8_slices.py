9eee23400091038e616369d064c47386d162b56d
Ramazan101 <ryskulovramazan8@gmail.com>
2026-08-06 20:01:29 +0600

    Initial commit: Today I repeat and learn function 'range()' and 'sciles' in python

---
+++ second_chapter_lists/8_slices.py
@@ -0,0 +1,16 @@
+players = ["Leonel Messi", "Cristiano Ronaldo", "Zlatan Ibrahimovic", "Neymar Junior", "Lamine Yamal", "Tom Hooland"]
+print(players[0:3])
+print(players[1:4])
+print(players[:5])
+print(players[2:])
+print(players[-3:])
+
+
+print("\nHere are the first three players on my team: ")
+for player in players[:3]:
+    print(f"This is {player}")
+    # pl = player[:3]
+
+
+
+
---
+++ second_chapter_lists/9_foods.py
@@ -0,0 +1,7 @@
+my_foods = ["pizza", "potato",  "beshbarmak", "carrot cake"]
+mym_foods = my_foods[:]
+
+
+
+print(f"My favorite foods are:\n{my_foods}\n")
+print(f"Mums favorite foods are:\n{mym_foods}")
